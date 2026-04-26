3710d81bdd75c7965148d31b9fa4a2bb2d0408c6
IDXGaza <idxgaza1122@gmail.com>
2026-04-26 16:11:00 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.idxgaza.wamda',
-  appName: 'وَمضة',
+  appName: 'ترانيم',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
