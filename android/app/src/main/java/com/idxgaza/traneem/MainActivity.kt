384d9b6969382bb0b5c9c9a0faa2c93fb1bb4c16
IDXGaza <idxgaza1122@gmail.com>
2026-06-01 00:50:35 +0300

    feat: register GoogleAuth plugin in MainActivity
    Update MainActivity to register the GoogleAuth plugin, enabling authentication support in the Android build pipeline.

---
+++ android/app/src/main/java/com/idxgaza/traneem/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.idxgaza.traneem
+
+import android.os.Bundle
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth
+import com.getcapacitor.BridgeActivity
+
+class MainActivity : BridgeActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        registerPlugin(GoogleAuth::class.java)
+        super.onCreate(savedInstanceState)
+    }
+}
